e6365a782a78f0d9b070ddc02e092bb7f99577af
timkeane <tkeane@doitt.nyc.gov>
2018-05-02 16:15:02 -0400

    .

--- src/js/map.js
+++ src/js/map.js
@@ -1,6 +1,21 @@
 var APP_ID = 'nyc-lib-example'
 var APP_KEY = '74DF5DB1D7320A9A2'
-var GEOCLIENT_URL = 'https://maps.nyc.gov/geoclient/v1/search.json?app_key=' + APP_KEY + '&app_id=' + APP_ID;
+
+var ID_FIELD = 'BoroCD';
+var FEATURE_NAME = {
+	/* implement for specific layer.json */
+	getName: function(){
+		var cd = this.get('BoroCD') + '';
+		var boro = {
+			'1': 'Manhattan',
+			'2': 'Bronx',
+			'3': 'Brooklyn',
+			'4': 'Queens',
+			'5': 'Staten Island'
+		}[cd.substr(0, 1)]
+		return boro + ' ' + cd.substr(1);
+	}
+}
 
 var map, source, layer, selectionSource, show;
 
@@ -17,7 +32,7 @@ if (qstr){
 
 function mapClicked(event){
 	map.forEachFeatureAtPixel(event.pixel, function(feature, layer){
-		var found = feature.get('BoroCD'); /* replace id with unigue id from data in layer.json */
+		var found = feature.get(ID_FIELD); /* replace id with unigue id from data in layer.json */
 		if (found && window.parent && window.parent.mapClicked){
 			window.parent.mapClicked(feature.getProperties());
 		}
@@ -47,7 +62,7 @@ $(document).ready(function(){
 
 	map = new nyc.ol.Basemap({target: $('#map').get(0)});
 
-	var geocoder = new nyc.Geoclient(GEOCLIENT_URL);
+	var geocoder = new nyc.Geoclient('https://maps.nyc.gov/geoclient/v1/search.json?app_key=' + APP_KEY + '&app_id=' + APP_ID);
 
 	var locationMgr = new nyc.LocationMgr({
 		controls: new nyc.ol.control.ZoomSearch(map),
@@ -58,23 +73,9 @@ $(document).ready(function(){
 
 	source = new nyc.ol.source.Decorating(
 		{url: 'layer.json', format: new ol.format.TopoJSON},
-		[{
-			extendFeature: function(){
-				this.setId(this.get('BoroCD')); /* set the feature id using the unique id from layer.json */
-			},
-			/* implement for specific layer.json */
-			getName: function(){
-				var cd = this.get('BoroCD') + '';
-				var boro = {
-					'1': 'Manhattan',
-					'2': 'Bronx',
-					'3': 'Brooklyn',
-					'4': 'Queens',
-					'5': 'Staten Island'
-				}[cd.substr(0, 1)]
-				return boro + ' ' + cd.substr(1);
-			}
-		}]
+		[FEATURE_NAME, {extendFeature: function(){
+				this.setId(this.get(ID_FIELD));
+			}}]
 	);
 	layer = new ol.layer.Vector({source: source, style: STYLE.layer});
 	map.addLayer(layer);
